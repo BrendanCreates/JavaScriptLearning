90a3faf65c6cfe11d10306a5536fe5378f55c9b6
BrendanCreates <124849622+BrendanCreates@users.noreply.github.com>
2025-07-17 11:52:22 -0400

    uploading a portion of past notes

---
+++ 0-1An_Introduction.js
@@ -0,0 +1,31 @@
+// An Introduction
+
+/*
+
+JavaScript is provided and executed as plain text so no preparation or compilation to run, initially made to make web pages active.
+
+It used to have the name LiveScript but they wanted to support Java since it was unpopular at the time so they called it JavaScript.
+
+It evolved into its own independent thing and has its own specification called ECMAScript and has quite literally NO relation to Java at this point
+
+
+JavaScript can execute in browser ofc, it can also execute on a server or ANY device that has the JavaScript engine
+
+Browsers have an embedded engine called Javascript virtual machine
+- Chrome, Opera, and Edge have the [V8](https://en.wikipedia.org/wiki/V8_(JavaScript_engine)) engine
+- Firefox has [SpiderMonkey](https://en.wikipedia.org/wiki/SpiderMonkey) engine
+- Safari has some but idc
+
+There will be some features that are supported by some engines and not others so its good to have a general idea for what the engines are
+
+*/
+
+// How Engines Work on a Basic Level
+
+/*
+
+
+
+
+
+*/
